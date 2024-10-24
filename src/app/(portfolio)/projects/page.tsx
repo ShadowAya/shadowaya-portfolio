e8870aa0f1b7d1e320378e667435e831dbbd3826
ShadowAya <jonhlad@gmail.com>
2024-10-24 22:44:19 +0200

    add stratle to projects page

--- src/app/(portfolio)/projects/page.tsx
+++ src/app/(portfolio)/projects/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
         <ComputerWindow
             top={200}
             left={20}
-            height={600}
+            height={680}
             width={330}
             delay={0.5}
         >
@@ -41,6 +41,15 @@ export default function Page() {
                     height={45}
                 />
                 <p>Makogate</p>
+            </InlineLink>
+            <InlineLink href={"/stratle/game"}>
+                <Image
+                    src="/stratle-icon.png"
+                    alt="stratle"
+                    width={45}
+                    height={45}
+                />
+                <p>Stratle</p>
             </InlineLink>
             <InlineLink href={"https://wiseway.cz/"}>
                 <Image
