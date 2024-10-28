e2bed540f20d32b5256d07e924b02a0c914d18e5
ShadowAya <jonhlad@gmail.com>
2024-10-28 15:15:02 +0100

    add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         </head>
         <body className={inter.className}>
             {children}
+            <SpeedInsights />
         </body>
     </html>
   )
