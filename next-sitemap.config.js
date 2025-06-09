18cb25f2bc69333344512198ca7472317413f77c
ShadowAya <jonhlad@gmail.com>
2025-06-09 22:15:01 +0200

    update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,9 @@
 /**
  * @typedef {'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never'} ChangeFrequency
- * 
- * @param {string} loc 
- * @param {ChangeFrequency} changefreq 
- * @param {number} priority 
+ *
+ * @param {string} loc
+ * @param {ChangeFrequency} changefreq
+ * @param {number} priority
  * @returns {
  *  loc: string,
  *  changefreq: ChangeFrequency,
@@ -23,20 +23,20 @@ function transformPath(loc, changefreq, priority) {
 // next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.shadowaya.me',
+    siteUrl: "https://www.shadowaya.me",
     generateRobotsTxt: true,
-    exclude: ['/stratle', '/stratle/library', '*.png'],
+    exclude: ["/stratle/game", "/stratle/library", "*.png"],
     transform: async (config, path) => {
         if (
-            path.startsWith('/lethal-calculator') ||
-            path.startsWith('/stratle') ||
-            path.startsWith('/kustom-kompiler')
+            path.startsWith("/lethal-calculator") ||
+            path.startsWith("/stratle") ||
+            path.startsWith("/kustom-kompiler")
         ) {
-            return transformPath(path, 'weekly', 0.5);
+            return transformPath(path, "weekly", 0.5);
         }
-        if (path === '/') {
-            return transformPath(path, 'daily', 0.9);
+        if (path === "/") {
+            return transformPath(path, "daily", 0.9);
         }
-        return transformPath(path, 'daily', 0.7);
+        return transformPath(path, "daily", 0.7);
     },
 };
