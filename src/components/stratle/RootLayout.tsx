4384cbfc8d3bd7f872b7fdce134b0c1bd4d3a46d
ShadowAya <jonhlad@gmail.com>
2025-06-09 22:02:59 +0200

    fix stratle, update

--- src/components/stratle/RootLayout.tsx
+++ src/components/stratle/RootLayout.tsx
@@ -10,65 +10,71 @@ import MoreInfo from "./MoreInfo";
 import moment from "moment-timezone";
 import PageWrap from "./PageWrap";
 import { getStratagemList } from "@/utils/stratle/utils";
+import { unstable_cacheLife as cacheLife } from "next/cache";
 
 interface LayoutProps {
-  children?: React.ReactNode;
+    children?: React.ReactNode;
 }
 
 function hashString(str: string) {
-  let hash = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + (hash << 6) + (hash << 16) - hash;
-  }
-  return hash >>> 0;
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + (hash << 6) + (hash << 16) - hash;
+    }
+    return hash >>> 0;
 }
 
-function getRandomIndexByDate(length: number) {
-  const currentDate = moment().tz("UTC").format("YYYYMMDD");
-  const hash = hashString(currentDate);
+async function getRandomIndexByDate(length: number) {
+    "use cache";
+    cacheLife("hours");
+    const currentDate = moment().tz("UTC").format("YYYYMMDD");
+    const hash = hashString(currentDate);
 
-  return hash % length;
+    return hash % length;
 }
 
 export default async function RootLayout({ children }: LayoutProps) {
-  const content = await getStratagemList();
-  const index = getRandomIndexByDate(content.length);
+    const content = await getStratagemList();
+    const index = await getRandomIndexByDate(content.length);
 
-  return (
-    <div className={styles.layout}>
-      <Image
-        src={"/hd2-bg.png"}
-        width={1920}
-        height={1080}
-        alt={"background"}
-      />
-      <StratagemProvider stratagems={content} todayIndex={index}>
-        <GameProvider>
-          <PageWrap>
-            <div>
-              <div className={styles.title}>
-                <Box className={styles.logo}>
-                  <Dashed absolute={"8px"} />
-                  <Image
-                    src={"/hd2-logo.png"}
-                    width={30}
-                    height={30}
-                    alt={"logo"}
-                  />
-                </Box>
-                <div className={styles.texts}>
-                  <h1>STRATLE</h1>
-                  <h2>A WORDLE GAME FOR HELLDIVERS 2 STRATAGEMS</h2>
-                </div>
-              </div>
-              <Nav />
-              <div className={styles.content}>{children}</div>
-            </div>
-          </PageWrap>
-          <TreasonWarning />
-          <MoreInfo />
-        </GameProvider>
-      </StratagemProvider>
-    </div>
-  );
+    return (
+        <div className={styles.layout}>
+            <Image
+                src={"/hd2-bg.png"}
+                width={1920}
+                height={1080}
+                alt={"background"}
+            />
+            <StratagemProvider stratagems={content} todayIndex={index}>
+                <GameProvider>
+                    <PageWrap>
+                        <div>
+                            <div className={styles.title}>
+                                <Box className={styles.logo}>
+                                    <Dashed absolute={"8px"} />
+                                    <Image
+                                        src={"/hd2-logo.png"}
+                                        width={30}
+                                        height={30}
+                                        alt={"logo"}
+                                    />
+                                </Box>
+                                <div className={styles.texts}>
+                                    <h1>STRATLE</h1>
+                                    <h2>
+                                        A WORDLE GAME FOR HELLDIVERS 2
+                                        STRATAGEMS
+                                    </h2>
+                                </div>
+                            </div>
+                            <Nav />
+                            <div className={styles.content}>{children}</div>
+                        </div>
+                    </PageWrap>
+                    <TreasonWarning />
+                    <MoreInfo />
+                </GameProvider>
+            </StratagemProvider>
+        </div>
+    );
 }
--- src/utils/stratle/utils.ts
+++ src/utils/stratle/utils.ts
@@ -2,233 +2,249 @@ import libxmljs from "libxmljs";
 import { unstable_cacheLife as cacheLife } from "next/cache";
 
 export type Stratagem = {
-  name: string;
-  icon: string | null;
-  permitType: string;
-  unlockLevel: string;
-  unlockCost: string;
-  module: string;
-  traits: string[];
-  code: ("up" | "right" | "left" | "down")[];
-  cooldown: number;
-  uses: number;
+    name: string;
+    icon: string | null;
+    permitType: string;
+    unlockLevel: string;
+    unlockCost: string;
+    module: string;
+    traits: string[];
+    code: ("up" | "right" | "left" | "down")[];
+    cooldown: number;
+    uses: number;
 };
 
 function parseArrow(arrowImg: string) {
-  if (arrowImg.includes("Up_Arrow")) return "up";
-  if (arrowImg.includes("Right_Arrow")) return "right";
-  if (arrowImg.includes("Down_Arrow")) return "down";
-  if (arrowImg.includes("Left_Arrow")) return "left";
-  return "up";
+    if (arrowImg.includes("Up_Arrow")) return "up";
+    if (arrowImg.includes("Right_Arrow")) return "right";
+    if (arrowImg.includes("Down_Arrow")) return "down";
+    if (arrowImg.includes("Left_Arrow")) return "left";
+    return "up";
 }
 
 export async function getStratagemList() {
-  "use cache";
-  cacheLife({
-    stale: 60 * 60 * 1, // 1 hour
-    revalidate: 60 * 60 * 24, // 24 hours
-    expire: 60 * 60 * 25, // 25 hours
-  });
+    "use cache";
+    cacheLife({
+        stale: 60 * 60 * 1, // 1 hour
+        revalidate: 60 * 60 * 24, // 24 hours
+        expire: 60 * 60 * 25, // 25 hours
+    });
 
-  const stratagems: Stratagem[] = [];
+    const stratagems: Stratagem[] = [];
 
-  const res = await fetch("https://helldivers.wiki.gg/wiki/Stratagems", {
-    cache: "no-store",
-  });
-  const html = await res.text();
-  // const html = fs.readFileSync('./content.txt', 'utf8');
-
-  const doc = await libxmljs.parseHtmlAsync(html);
-
-  const stratagemLinks = doc
-    .find("//table[contains(@class, 'wikitable')][1]/tbody/tr/td[2]/a/@href")
-    .map(
-      (a) =>
-        ("https://helldivers.wiki.gg" +
-          (a.child(0)?.text() ?? "")) as `https://helldivers.wiki.gg${string}`
-    );
-
-  for (const link of stratagemLinks) {
-    const res = await fetch(link, { cache: "no-store" });
+    const res = await fetch("https://helldivers.wiki.gg/wiki/Stratagems", {
+        cache: "no-store",
+    });
     const html = await res.text();
-    // const html = fs.readFileSync('./content2.txt');
+    console.log("💀 Fetched Stratagems page");
+    // const html = fs.readFileSync('./content.txt', 'utf8');
 
-    const doc = libxmljs.parseHtml(html);
-    const table = doc.find(
-      "//aside[contains(@class, 'portable-infobox')][1]"
-    )[0];
-    const icon =
-      doc
+    const doc = await libxmljs.parseHtmlAsync(html);
+
+    console.log("💀 Getting stratagem links");
+    const stratagemLinks = doc
         .find(
-          "//aside[contains(@class, 'portable-infobox')][1]//img[@class='pi-image-thumbnail']/@src"
-        )[0]
-        .child(0)
-        ?.text() ?? null;
-    const content = table.find(".//div[contains(@class, 'pi-data-value')]");
+            "//table[contains(@class, 'wikitable')][1]/tbody/tr/td[2]/a/@href"
+        )
+        .map(
+            (a) =>
+                ("https://helldivers.wiki.gg" +
+                    (a.child(0)?.text() ??
+                        "")) as `https://helldivers.wiki.gg${string}`
+        );
+    console.log(`💀 Found ${stratagemLinks.length} stratagem links`);
 
-    const statsTableList = doc.find(
-      "//table[contains(@class, 'wikitable')]/tbody"
-    );
-    let statsTableI = 0;
-    let statsTable: string[];
-    do {
-      statsTable = statsTableList[statsTableI]
-        .childNodes()
-        .map((e) => e.text());
-      statsTableI++;
-    } while (!statsTable.some((a) => a.includes("Uses")));
+    let counter = 1;
+    for (const link of stratagemLinks) {
+        const res = await fetch(link, { cache: "no-store" });
+        console.log(`  💀 ${counter++} Fetching stratagem: ${link}`);
+        const html = await res.text();
+        // const html = fs.readFileSync('./content2.txt');
 
-    let usesString = statsTable.find((a) => a.startsWith("\nUses")) ?? "";
-    usesString =
-      (usesString.includes("Standard")
-        ? /^(.|\n)+Standard(\n)+(?<res>.+)(\n)+$/g.exec(usesString)?.groups?.res
-        : /^(.|\n)+Uses(\n)+(?<res>.+)(\n)+$/g.exec(usesString)?.groups?.res) ??
-      "Unlimited";
-    const uses =
-      usesString === "Unlimited"
-        ? 0
-        : parseInt(usesString.replace(" seconds", ""));
+        const doc = libxmljs.parseHtml(html);
+        const table = doc.find(
+            "//aside[contains(@class, 'portable-infobox')][1]"
+        )[0];
+        const icon =
+            doc
+                .find(
+                    "//aside[contains(@class, 'portable-infobox')][1]//img[@class='pi-image-thumbnail']/@src"
+                )[0]
+                .child(0)
+                ?.text() ?? null;
+        const content = table.find(".//div[contains(@class, 'pi-data-value')]");
 
-    let cooldownString =
-      statsTable.find((a) => a.startsWith("\nCooldown")) ?? "";
-    cooldownString =
-      /^(.|\n)+Standard(\n)+(?<res>.+)(\n)+$/g.exec(cooldownString)?.groups
-        ?.res ?? "Unlimited";
-    const cooldown =
-      cooldownString === "Unlimited"
-        ? 0
-        : parseInt(cooldownString.replace(/\D/g, ""));
+        const statsTableList = doc.find(
+            "//table[contains(@class, 'wikitable')]/tbody"
+        );
+        let statsTableI = 0;
+        let statsTable: string[] = [];
+        do {
+            try {
+                statsTable = statsTableList[statsTableI]
+                    .childNodes()
+                    .map((e) => e.text());
+                statsTableI++;
+            } catch {}
+        } while (
+            !statsTable.some((a) => a.includes("Uses")) &&
+            statsTableI < statsTableList.length
+        );
 
-    const entry = (
-      content.length == 6
-        ? {
-            permitType: content[0].child(0)?.text(),
-            unlockLevel: content[1].child(0)?.text(),
-            unlockCost: content[2].child(0)?.text(),
-            module: content[3].child(0)?.text(),
-            traits: content[4]
-              .child(0)
-              ?.text()
-              .split(/ ? ?• ? ?/),
-            code: content[5].find(".//img/@src").map((a) =>
-              parseArrow(
-                a
-                  .toString()
-                  .replace(/^\s+src="/, "")
-                  .replace(/"$/, "")
-              )
-            ),
-          }
-        : content.length == 5
-        ? {
-            permitType: content[0].child(0)?.text(),
-            unlockLevel: content[1].child(0)?.text(),
-            unlockCost: content[2].child(0)?.text(),
-            module: "Unknown",
-            traits: content[3].child(0)?.text().split("  •  "),
-            code: content[4].find(".//img/@src").map((a) =>
-              parseArrow(
-                a
-                  .toString()
-                  .replace(/^\s+src="/, "")
-                  .replace(/"$/, "")
-              )
-            ),
-          }
-        : content[1].child(0)?.text() == "?"
-        ? {
-            permitType: content[0].child(0)?.text(),
-            unlockLevel: "?",
-            unlockCost: "?",
-            module: content[2].child(0)?.text(),
-            traits: content[3].child(0)?.text().split("  •  "),
-            code: content[4].find(".//img/@src").map((a) =>
-              parseArrow(
-                a
-                  .toString()
-                  .replace(/^\s+src="/, "")
-                  .replace(/"$/, "")
-              )
-            ),
-          }
-        : {
-            permitType: content[0].child(0)?.text(),
-            unlockLevel: "1",
-            unlockCost: content[1]
-              .child(0)
-              ?.text()
-              .replace(/^\s+/g, "")
-              .replace("Medal", "Medals"),
-            module: "Warbond",
-            traits: content[2].child(0)?.text().split("  •  "),
-            code: content[3].find(".//img/@src").map((a) =>
-              parseArrow(
-                a
-                  .toString()
-                  .replace(/^\s+src="/, "")
-                  .replace(/"$/, "")
-              )
-            ),
-          }
-    ) as Stratagem;
+        let usesString = statsTable.find((a) => a.startsWith("\nUses")) ?? "";
+        usesString =
+            (usesString.includes("Standard")
+                ? /^(.|\n)+Standard(\n)+(?<res>.+)(\n)+$/g.exec(usesString)
+                      ?.groups?.res
+                : /^(.|\n)+Uses(\n)+(?<res>.+)(\n)+$/g.exec(usesString)?.groups
+                      ?.res) ?? "Unlimited";
+        const uses =
+            usesString === "Unlimited"
+                ? 0
+                : parseInt(usesString.replace(" seconds", ""));
 
-    entry.name =
-      table
-        .get("h2")
-        ?.toString()
-        .replace(/<[^>]+>/g, "") ?? "";
-    entry.icon = icon;
+        let cooldownString =
+            statsTable.find((a) => a.startsWith("\nCooldown")) ?? "";
+        cooldownString =
+            /^(.|\n)+Standard(\n)+(?<res>.+)(\n)+$/g.exec(cooldownString)
+                ?.groups?.res ?? "Unlimited";
+        const cooldown =
+            cooldownString === "Unlimited"
+                ? 0
+                : parseInt(cooldownString.replace(/\D/g, ""));
 
-    entry.uses = uses;
-    entry.cooldown = cooldown;
+        const entry = (
+            content.length == 6
+                ? {
+                      permitType: content[0].child(0)?.text(),
+                      unlockLevel: content[1].child(0)?.text(),
+                      unlockCost: content[2].child(0)?.text(),
+                      module: content[3].child(0)?.text(),
+                      traits: content[4]
+                          .child(0)
+                          ?.text()
+                          .split(/ ? ?• ? ?/),
+                      code: content[5].find(".//img/@src").map((a) =>
+                          parseArrow(
+                              a
+                                  .toString()
+                                  .replace(/^\s+src="/, "")
+                                  .replace(/"$/, "")
+                          )
+                      ),
+                  }
+                : content.length == 5
+                ? {
+                      permitType: content[0].child(0)?.text(),
+                      unlockLevel: content[1].child(0)?.text(),
+                      unlockCost: content[2].child(0)?.text(),
+                      module: "Unknown",
+                      traits: content[3].child(0)?.text().split("  •  "),
+                      code: content[4].find(".//img/@src").map((a) =>
+                          parseArrow(
+                              a
+                                  .toString()
+                                  .replace(/^\s+src="/, "")
+                                  .replace(/"$/, "")
+                          )
+                      ),
+                  }
+                : content[1].child(0)?.text() == "?"
+                ? {
+                      permitType: content[0].child(0)?.text(),
+                      unlockLevel: "?",
+                      unlockCost: "?",
+                      module: content[2].child(0)?.text(),
+                      traits: content[3].child(0)?.text().split("  •  "),
+                      code: content[4].find(".//img/@src").map((a) =>
+                          parseArrow(
+                              a
+                                  .toString()
+                                  .replace(/^\s+src="/, "")
+                                  .replace(/"$/, "")
+                          )
+                      ),
+                  }
+                : {
+                      permitType: content[0].child(0)?.text(),
+                      unlockLevel: "1",
+                      unlockCost: content[1]
+                          .child(0)
+                          ?.text()
+                          .replace(/^\s+/g, "")
+                          .replace("Medal", "Medals"),
+                      module: "Warbond",
+                      traits: content[2].child(0)?.text().split("  •  "),
+                      code: content[3].find(".//img/@src").map((a) =>
+                          parseArrow(
+                              a
+                                  .toString()
+                                  .replace(/^\s+src="/, "")
+                                  .replace(/"$/, "")
+                          )
+                      ),
+                  }
+        ) as Stratagem;
 
-    stratagems.push(entry);
-  }
+        entry.name =
+            table
+                .get("h2")
+                ?.toString()
+                .replace(/<[^>]+>/g, "") ?? "";
+        entry.icon = icon;
 
-  // const missionStratagemTraits = doc.find("//table[contains(@class, 'wikitable')][2]/tbody/tr/th").slice(5);
-  const missionStratagems = doc
-    .find("//table[contains(@class, 'wikitable')][2]/tbody/tr")
-    .slice(1)
-    .map((a) => a.childNodes());
+        entry.uses = uses;
+        entry.cooldown = cooldown;
 
-  let lastTrait = "";
-
-  const usesMap = {
-    Reinforce: 5,
-    "SoS Beacon": 1,
-  };
-
-  const cooldownMap = {
-    "SEAF Artillery": 11,
-    Ressuply: 180,
-    Hellbomb: 30,
-  };
-
-  const missionStratagemsParsed = missionStratagems.map((a) => {
-    if (a.length == 10) {
-      lastTrait = a[1].text().replace("\n", "");
-      a = a.slice(2);
+        stratagems.push(entry);
     }
-    const name = a[3].text().replace("\n", "");
-    const entry: Stratagem = {
-      icon: a[1].find(".//img/@src")[0]?.child(0)?.text() ?? null,
-      name,
-      permitType: "Mission Stratagem",
-      unlockLevel: "1",
-      unlockCost: "Already Unlocked",
-      module: "Mission",
-      traits: [lastTrait],
-      code: a[5]
-        .find(".//img/@src")
-        .map((n) => parseArrow(n.child(0)?.text() ?? "")),
-      uses: name in usesMap ? usesMap[name as keyof typeof usesMap] : 0,
-      cooldown:
-        name in cooldownMap ? cooldownMap[name as keyof typeof cooldownMap] : 0,
+
+    // const missionStratagemTraits = doc.find("//table[contains(@class, 'wikitable')][2]/tbody/tr/th").slice(5);
+    const missionStratagems = doc
+        .find("//table[contains(@class, 'wikitable')][2]/tbody/tr")
+        .slice(1)
+        .map((a) => a.childNodes());
+
+    let lastTrait = "";
+
+    const usesMap = {
+        Reinforce: 5,
+        "SoS Beacon": 1,
     };
-    return entry;
-  });
 
-  stratagems.push(...missionStratagemsParsed);
+    const cooldownMap = {
+        "SEAF Artillery": 11,
+        Ressuply: 180,
+        Hellbomb: 30,
+    };
 
-  return stratagems;
+    const missionStratagemsParsed = missionStratagems.map((a) => {
+        if (a.length == 10) {
+            lastTrait = a[1].text().replace("\n", "");
+            a = a.slice(2);
+        }
+        const name = a[3].text().replace("\n", "");
+        const entry: Stratagem = {
+            icon: a[1].find(".//img/@src")[0]?.child(0)?.text() ?? null,
+            name,
+            permitType: "Mission Stratagem",
+            unlockLevel: "1",
+            unlockCost: "Already Unlocked",
+            module: "Mission",
+            traits: [lastTrait],
+            code: a[5]
+                .find(".//img/@src")
+                .map((n) => parseArrow(n.child(0)?.text() ?? "")),
+            uses: name in usesMap ? usesMap[name as keyof typeof usesMap] : 0,
+            cooldown:
+                name in cooldownMap
+                    ? cooldownMap[name as keyof typeof cooldownMap]
+                    : 0,
+        };
+        return entry;
+    });
+
+    stratagems.push(...missionStratagemsParsed);
+
+    return stratagems;
 }
